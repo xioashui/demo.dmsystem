a6a71315b157df467e6b708cb80b016979379306
lpz <1274650479@qq.com>
2021-09-20 09:42:06 +0800

    put a test for jdbc

--- src/test/java/com/demo/dmsystem/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/dmsystem/demo/DemoApplicationTests.java
@@ -1,15 +1,29 @@
 package com.demo.dmsystem.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+    /*
+    author:李平喆
+    time:2021/9/20
+    describe:这是一个测试数据库连接，以及数据源的测试方法
+    * */
     @Test
-    void contextLoads() {
-        System.out.println("hello word!");
-        System.out.println("hello word!");
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection=dataSource.getConnection();
+        connection.close();
     }
-
 }
